f43d3722910d221469a063dd43d5db617ab61874
RodrigoVaillant <leshakostunov2006@gmail.com>
2024-12-25 00:05:50 +0200

    added footer, added animation on button

---
+++ frontend/src/components/UI/Footer.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+</script>
+
+<template>
+
+<div class="footer w-full h-56 text-1xl px-12 bg-gray-900 text-white flex justify-between items-center rounded-b-2xl">
+
+    <figure class="flex flex-row text-gray-400">
+        <ul class="space-y-4">
+            <li class="text-3xl text-white pb-2">Contacts</li>
+            <li class="animated"><a href="">Instagram</a></li>
+            <li class="animated"><a href="">Telegram</a></li>
+            <li class="animated"><a href="">Facebook</a></li>
+        </ul>
+    </figure>
+    <figure class="flex flex-row text-gray-400">
+        <ul class="space-y-4">
+            <li class="text-3xl text-white pb-2">To explore</li>
+            <li class="animated"><a href="">Blog</a></li>
+            <li class="animated"><a href="">Experiences</a></li>
+            <li class="animated"><a href="">Accommodations</a></li>
+        </ul>
+    </figure>
+    <figure class="flex flex-row text-gray-400">
+        <ul class="space-y-4">
+            <li class="text-3xl text-white pb-2">Help and services</li>
+            <li class="animated"><a href="">FAQS</a></li>
+            <li class="animated"><a href="">Legal Warning</a></li>
+            <li class="animated"><a href="">How does it work</a></li>
+        </ul>
+    </figure>
+    <figure class="flex flex-row text-gray-400">
+        <ul class="space-y-4">
+            <li class="text-3xl text-white pb-2">Hot links</li>
+            <li class="animated"><a href="">Home</a></li>
+            <li class="animated"><a href="">Shop</a></li>
+            <li class="animated"><a href="">Blog</a></li>
+        </ul>
+    </figure>
+
+</div>
+
+</template>
+
+<style scoped>
+.animated {
+  @apply transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-slate-900 duration-300;
+};
+
+/* .liName{
+    @apply text-3xl text-white p-x-2;
+} */
+</style>
--- frontend/src/components/UI/PreviewCards.vue
+++ frontend/src/components/UI/PreviewCards.vue
@@ -1,13 +1,18 @@
 <script setup>
+import Footer from '@/components/UI/Footer.vue';
 
 </script>
 
 <template>
 
-    <div class="bg-white w-full h-screen flex justify-center py-9 flex-col">
-        
+
+    <div class="bg-white w-full flex flex-col rounded-b-2xl">
+    <div class="flex justify-center">
+        <p class="text-center my-12 text-2xl w-2/3">Місце, де стиль зустрічається з індивідуальністю. У нашому магазині ми зібрали культові бренди, які стали символами сучасної моди. <b>Overload</b> – це більше, ніж одяг, це спосіб життя, який підкреслює твою унікальність.
+            Від <b>Stone Island</b> до <b>C.P Company</b> – обирай те, що резонує з твоїм характером. Створюй образи, які надихають, разом з <b>OverLoad</b></p>
+    </div>
         <!-- Choice brand block -->
-        <div class="w-full h-2/3 flex justify-around mx-6">
+        <div class="w-full h-2/3 flex justify-around">
             <!-- Block banner -->
             <figure class="bg-white w-1/3 h-3/3 m-10 flex justify-center items-center flex-col border-black border-2 rounded-xl">
                 <!-- Photo -->
@@ -15,8 +20,12 @@
                     <img src="../../icons/stoneisland.svg" alt="" class="max-w-full h-auto">
                 </div>
                 <h1 class="text-2xl text-center">Stone Island</h1>
-                <figcaption class="text-2xl text-center">Stone Island — це італійський бренд, який поєднує інноваційні технології, якісні матеріали та неповторний стиль. </figcaption>
-                
+                <figcaption class="text-2xl text-center"><b>Stone Island</b> — це італійський бренд, який поєднує інноваційні технології, якісні матеріали та неповторний стиль. </figcaption>
+                <router-link 
+                to="/store/CpCompany" 
+                class="text-2xl rounded-xl bg-black text-white w-48 h-24 m-4 border-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-slate-900 duration-300 flex items-center justify-center">
+                Get Style
+                </router-link>
                 
             </figure>
 
@@ -25,12 +34,16 @@
                 <div class=" w-2/3 h-2/3 flex justify-center">
                     <img src="../../icons/C.P._Company_logo.svg" alt="" class="max-w-full h-auto">
                 </div>
-                <h1 class="text-2xl text-center">Stone Island</h1>
-                <figcaption class="text-2xl text-center">Stone Island — це італійський бренд, який поєднує інноваційні технології, якісні матеріали та неповторний стиль. </figcaption>
-                <router-link to="store/CpCompany" class="text-2xl text-center items-center inline-flex rounded-xl bg-black text-white w-48 h-24 m-4 border-2">Enjoi CP Company</router-link>
-                
+                <h1 class="text-2xl text-center">C.P Company</h1>
+                <figcaption class="text-2xl text-center"><b>C.P Company</b> — це італійський бренд, який відомий своїм поєднанням функціональності та стилю. </figcaption>
+                <router-link 
+                to="/store/CpCompany" 
+                class="text-2xl rounded-xl bg-black text-white w-48 h-24 m-4 border-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-slate-900 duration-300 flex items-center justify-center">
+                Get Style
+                </router-link>
             </figure>
         </div>
+    <Footer/>
     </div>
 
     
