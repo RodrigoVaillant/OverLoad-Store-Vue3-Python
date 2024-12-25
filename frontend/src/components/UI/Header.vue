82b37c18479abee592a5682197e7327c3af55b76
RodrigoVaillant <leshakostunov2006@gmail.com>
2024-12-25 11:18:46 +0200

    fix nav bar

--- frontend/src/components/UI/Header.vue
+++ frontend/src/components/UI/Header.vue
@@ -5,15 +5,14 @@
 <template>
     <div class=" m-0 p-0 border-b-2">
         <div class="bg-white w-full rounded-t-2xl">
-            <ul class="flex justify-around flex-row text-3xl p-5 ml-5 w-full h-35">
-                <li class="flex items-center "><a href="#" class="text-4xl font-semibold">Overload</a></li>
+            <ul class="flex justify-around items-center flex-row text-3xl p-5 ml-5 w-full h-35">
+                <li class=""><a href="#" class="text-4xl font-semibold">Overload</a></li>
                 <router-link to="/" class="ease-out duration-300 hover:text-zinc-500">Home</router-link>
-                <router-link to="/buyer-service" class="ease-out duration-300 hover:text-zinc-500">Buyer
-                    Service</router-link>
+                <router-link to="/buyer-service" class="ease-out duration-300 hover:text-zinc-500">BuyerService</router-link>
                 <router-link to="/new" class="ease-out duration-300 hover:text-zinc-500">New</router-link>
                 <router-link to="/login" tag="img"><img src="../../icons/accountHeader.svg" alt="" class="filter hover:out"></router-link>
-
             </ul>
+
         </div>
     </div>
 </template>
