891abd7c8477f3c94e1bfa99bc3dd26053257ff2
RodrigoVaillant <leshakostunov2006@gmail.com>
2024-12-25 11:53:29 +0200

    added login page, added input field

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/components/Home.vue'
 import BuyerService from '@/components/BuyerService.vue'
 import New from '@/components/New.vue'
+import Login from '@/components/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,7 @@ const router = createRouter({
     {path: "/", component: Home},
     {path: "/buyer-service", component: BuyerService},
     {path: "/new", component: New},
+    {path: "/login", component: Login},
   ],
 })
 
